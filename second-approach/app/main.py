df65947d6b7cb3d0106cd1f654b2bf7dbbf5f23d
Adam Jensen <16873632+adamjosephjensen@users.noreply.github.com>
2025-01-19 09:33:42 -0600

    use env variable for host when sending emails

--- second-approach/app/main.py
+++ second-approach/app/main.py
@@ -12,7 +12,6 @@ app = create_app()
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-# Configure server name for production
-if os.environ.get('FLASK_ENV') == 'production':
-    app.config['SERVER_NAME'] = os.environ.get('SERVER_NAME', '18.224.56.202.nip.io')
+# Configure server name for all environments
+app.config['SERVER_NAME'] = os.environ.get('SERVER_NAME', 'localhost:5000')
 
